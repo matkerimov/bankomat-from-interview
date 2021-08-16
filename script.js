476f90ec42aa32dce5e5b5e96c7081fdb54f1488
zhaiyl07 <matkerimov.zhaiyl.ib9.1.19@gmail.com>
2021-08-16 21:46:46 +0600

    sokrasheniye

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const terminal = (num) =>{
-    let banknotes = [100, 50, 20, 5, 1]
-    let result = []
-    if (num > 0){
-        for (let i = 0; i < banknotes.length; i++){
-            let note = banknotes[i]
-            while(num - note >= 0){
-                num -= note
-                result.push(note)
-            }
-        }
-    }else {
-        console.log( "Please enter new num " )
-    }
-    return result
-}
-console.log(terminal(357))
+// const terminal = (num) =>{
+//     let banknotes = [100, 50, 20, 5, 1]
+//     let result = []
+//         for (let i = 0; i < banknotes.length; i++){
+//             while(num - banknotes[i] >= 0){
+//                 num -= banknotes[i]
+//                 result.push(banknotes[i])
+//             }
+//         }
+//     return result
+// }
+// console.log(terminal(357))
+
